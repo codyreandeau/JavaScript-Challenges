bf7c386d498f2aca11994839be6ff57ce53509c4
Cody Reandeau <codyreandeau@yahoo.com>
2016-11-04 15:03:25 -0400

    function that truncates '...' to a string, with respect to the decision statements in the function.

---
+++ truncate_string.js
@@ -0,0 +1,15 @@
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+if(str.length > num){
+  if(num <= 3){
+    return str.slice(0, num) + "...";
+   }else{
+     return str.slice(0, num-3) + "...";
+    }
+ }
+  if(str.length <= num){
+   return str;
+ }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
