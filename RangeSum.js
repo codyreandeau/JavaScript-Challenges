32bf2a7842f60dfb23885fd8d6de1568a1959fc1
Cody Reandeau <codyreandeau@yahoo.com>
2016-12-25 22:01:01 -0500

    Function that adds up the numbers in the range of two specified numbers.

---
+++ RangeSum.js
@@ -0,0 +1,14 @@
+function sumAll(arr) {
+  
+  var x = Math.min(arr[0], arr[1]);
+  var y = Math.max(arr[0], arr[1]);
+  var sum = 0;
+  
+  for(x; x <= y; x++){
+    sum += x;
+  }
+  
+  return sum;
+}
+
+sumAll([1, 4]);
